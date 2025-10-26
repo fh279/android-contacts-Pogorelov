0a88e1dae87e0749381da6cd4db7d5be069873b9
mikhail.pogorelov <mikhail.pogorelov@myoffice.team>
2025-10-26 12:41:23 +0300

    Delete extra comment

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -7,14 +7,6 @@ import androidx.recyclerview.widget.DiffUtil;
 
 import ru.yandex.practicum.contacts.presentation.main.ContactUi;
 
-/**
- * Класс BaseListDiffCallback должен быть дженерик-классом с ограничением параметра типа
- * на обязательную реализацию интерфейса ListDiffInterface<>. В качестве параметра типа у интерфейса
- * указывается такое же имя, что и у класса.
-
- * Все типы данных, передаваемые в переопределяемые методы, должны быть заменены на дженерик-тип.
- */
-
 public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T> {
 
     @Override
